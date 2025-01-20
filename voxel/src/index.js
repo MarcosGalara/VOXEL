aa84751a93c6eb1d44b033ba014198800307ba56
MarcosGalara <marcos.galara08@gmail.com>
2025-01-20 18:56:16 -0300

    Pasos previos a iniciar proyecto

---
+++ voxel/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
+
+
+ReactDOM.render(
+  <Provider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
