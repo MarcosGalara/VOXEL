74574929d5a3e9466e169b1e3ba057c096412d8f
MarcosGalara <marcos.galara08@gmail.com>
2025-01-30 15:20:49 -0300

    Faltan detalles nomas

--- voxel/src/index.js
+++ voxel/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store'; // Asegúrate de importar tu store
+import "./global.css";
 
 import App from './App';
 
--- voxel/src/views/Home/Home.jsx
+++ voxel/src/views/Home/Home.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import MiLetrero from "../../assets/Untitled.jpg";
 import MiLaser from "../../assets/laser.jpg";
 import MiCorporeo from "../../assets/corporeo.jpg";
-import MiGrabado from "../../assets/grabado.jpg"
+import MiGrabado from "../../assets/grabado.jpg";
 
 const trabajos = [
-  { id: 1,  nombre: 'Letreros', imagen: MiLetrero,  },
-  { id: 2,  nombre: 'Corte Laser Metales',  imagen: MiLaser,  },
-  { id: 3,  nombre: 'Corte Laser Acrilicos',  imagen: MiCorporeo, },
-  { id: 4,  nombre: 'Grabados', imagen: MiGrabado,  },
+  { id: 1, nombre: 'Letreros', imagen: MiLetrero, ruta: '/letreros' },
+  { id: 2, nombre: 'Corte Laser Metales', imagen: MiLaser, ruta: '/corte-laser-metales' },
+  { id: 3, nombre: 'Corte Laser Acrilicos', imagen: MiCorporeo, ruta: '/corte-laser-acrilicos' },
+  { id: 4, nombre: 'Grabados', imagen: MiGrabado, ruta: '/grabados' },
 ];
 
 const Home = () => {
+  const navigate = useNavigate(); 
+
   return (
     <div className="container">
       <div className="card-container">
         {trabajos.map((trabajo) => (
-          <div key={trabajo.id} className="card">
+          <div
+            key={trabajo.id}
+            className="card"
+            onClick={() => navigate(trabajo.ruta)}
+          >
             <img src={trabajo.imagen} alt={trabajo.nombre} className="card-image" />
             <h3 className="card-title">{trabajo.nombre}</h3>
           </div>
